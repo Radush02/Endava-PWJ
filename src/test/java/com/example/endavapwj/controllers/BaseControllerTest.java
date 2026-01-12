413c7366ef1829eceb6e5279668df3a3f69b0020
Radu Cordunianu <82465411+Radush02@users.noreply.github.com>
2026-01-12 19:36:16 +0200

    Update BaseControllerTest.java

--- src/test/java/com/example/endavapwj/controllers/BaseControllerTest.java
+++ src/test/java/com/example/endavapwj/controllers/BaseControllerTest.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 public abstract class BaseControllerTest {
 
   @Autowired protected MockMvc mvc;
